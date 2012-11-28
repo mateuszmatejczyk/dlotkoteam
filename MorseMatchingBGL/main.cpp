fcd7a1ceeafd993c73995cc9bca5ff67ecb565bb
IIUJ-MaciejZgliczynski <zglicz@gmail.com>
2012-11-28 12:14:31 +0100

    Zmienilem sygnature funckji tak by byla dalo sie iterowac to z polaczeniem z algorytmem Macka mogla nam dac Algorytm 2 ( w pracy Dr Dlotki)

--- MorseMatchingBGL/main.cpp
+++ MorseMatchingBGL/main.cpp
@@ -6,31 +6,30 @@ using namespace std;
 
 //#define DEBUG
 
-void init_set(SympleksToGraphConvereter & converter, set<pair<int, vertex> > & unmarked, map<vertex,int> degs){
-	int n  = converter.vert.size();
+void init_set(SympleksToGraphConvereter & converter, Graph & g,set<pair<int, vertex> > & unmarked){
+	/*int n  = converter.vert.size();
 	
 	for(int i = 0 ; i < n ; ++i){
 		for(list<vertex>::iterator it = converter.vert[i].begin(); it!= converter.vert[i].end() ; ++it){
 			unmarked.insert(make_pair(i,*it));
-			degs[*it] = i;
 		}
+	}*/
+	
+	std::pair<vertex_iter, vertex_iter> p = vertices(g);
+	for(;p.first!=p.second; ++(p.first)){
+		unmarked.insert(make_pair( converter.inverseSympleksMap[*(p.first)].size() -1,*(p.first)));
 	}
 }
 
-void morseMatching(SympleksToGraphConvereter & converter, vector<vertex> & critical){
+void morseMatching(SympleksToGraphConvereter & converter, Graph & g, vector<vertex> & critical){
 	set<pair<int,vertex> > unmarked;
-	map<vertex,int> degs;
-	init_set(converter, unmarked, degs);
-	IndexMap index = get(boost::vertex_index, converter.g);
+	init_set(converter, g,unmarked);
+	IndexMap index = get(boost::vertex_index, g);
 	typedef boost::graph_traits < Graph >::adjacency_iterator adjacency_iterator;
-	
-	
 	out_edge_it ei, ei_end, possible_edge;
-	
 	set<pair<int,vertex> >::iterator temp;
 	set<pair<int,vertex> >::iterator possible;
 	while(!unmarked.empty()){
-		
 		set<pair<int , vertex> >::iterator  crit = unmarked.begin();
 		critical.push_back(crit->second);
 		//cout << unmarked.size() << " " << " " << crit->second << endl;
@@ -51,8 +50,8 @@ void morseMatching(SympleksToGraphConvereter & converter, vector<vertex> & criti
 					int counter = 0;
 					int currentDeg = it->first;
 					
-					for(boost::tie(ei,ei_end) = out_edges(it->second,converter.g) ; ei!= ei_end ; ++ei){
-						vertex v = boost::target(*ei,converter.g);
+					for(boost::tie(ei,ei_end) = out_edges(it->second,g) ; ei!= ei_end ; ++ei){
+						vertex v = boost::target(*ei,g);
 						temp = unmarked.find(make_pair(currentDeg - 1, v));
 						if(temp!=unmarked.end()){
 							
@@ -69,7 +68,7 @@ void morseMatching(SympleksToGraphConvereter & converter, vector<vertex> & criti
 					
 					if(counter == 1){ // hura udalo sie 
 						#ifdef DEBUG
-							cout << index[it->second] << " " << index[target(*ei,converter.g)] << endl; 
+							cout << index[it->second] << " " << index[target(*ei,g)] << endl; 
 						#endif
 						removeLast = true;
 						last = it;
@@ -78,8 +77,8 @@ void morseMatching(SympleksToGraphConvereter & converter, vector<vertex> & criti
 						unmarked.erase(possible);
 						// odwracanie krawedzi
 						//converter.g.remove_edge(possible_edge);
-						remove_edge(possible_edge,converter.g);
-						boost::add_edge(possible->second, it->second,converter.g);
+						remove_edge(possible_edge,g);
+						boost::add_edge(possible->second, it->second,g);
 						existPair = true;
 						removeLast =true;
 					}
@@ -89,7 +88,7 @@ void morseMatching(SympleksToGraphConvereter & converter, vector<vertex> & criti
 	
 }
 
-bool isAcyclic(Graph g){
+bool isAcyclic(Graph & g){
 	try{
 		typedef std::list<vertex> MakeOrder;
 		MakeOrder make_order;
@@ -108,7 +107,7 @@ int main(){
 	stgc.writeLegend("legend.txt");
 	cout << "read graph is acyclic " << isAcyclic(stgc.g) << endl;
 	vector<vertex> critical;
-	morseMatching(stgc,critical);
+	morseMatching(stgc,stgc.g,critical);
 	cout << " is acyclic " << isAcyclic(stgc.g) << endl;
 	cout << " Critical cells " << endl;
 	for(vector<vertex>::iterator it = critical.begin() ; it!= critical.end() ; ++it){
